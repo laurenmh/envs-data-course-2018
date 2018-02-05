4663a45f2c3feed5df6cefb87275286e6bd194d8
Lauren Hallett <hallett@uoregon.edu>
2018-02-05 08:14:00 -0800

    cosmetic changes to 08

--- 08_workflows/workflow_script.R
+++ 08_workflows/workflow_script.R
@@ -24,7 +24,6 @@ wtemp <- mutate(wtemp, date2 = mdy(date), month = month(date2), year = year(date
 ## 6) dplyr tool number 6: group_by
 ###################################
 
-## That last one was supposed to be a leading question. I don't think mean temperature is that insightful. 
 ## I'm more interested in how temperature changes with month or year.
 ## If we add the group_by function, summarize will give us the requested value FOR EACH GROUP.
 
@@ -121,10 +120,10 @@ Calispell_monthtemp
 
 ## WORKFLOW: Mimic Figure 1 (except on the y-axis just plot species richness).
 ## Metadata here: http://www.cedarcreek.umn.edu/research/data/dataset?ple001
-## With a partner, write out the workflow you will use in English and put up a green sticky.
+## With a partner, write out the workflow you will use in English and put up a green sticky when done.
     # What QAQC will you do beforehand? 
     # How will you manipulate the data once you are happy with it?
-## After we have discussed as a class, write out and run the workflow in tidyverse R
+## After we have discussed as a class, write out and run the workflow in tidyverse R.
 
 ## Here's the data import to get you going:
 cdrplant <- read.csv("e001_Plant aboveground biomass data.csv", stringsAsFactors = F, skip = 726) %>%
@@ -140,11 +139,11 @@ cdrplant <- read.csv("e001_Plant aboveground biomass data.csv", stringsAsFactors
 
 ## WORKFLOW: Mimic a panel in Figure 7 for C:N ratio of above ground biomass with nitrogen addition level
 ## Metadata here: http://www.cedarcreek.umn.edu/research/data/dataset?nbe001
-## With a partner, write out the workflow you will use in English and put up a green sticky
+## With a partner, write out the workflow you will use in English and put up a green sticky when you are done.
    # What QAQC will you do beforehand? 
    # How will you manipulate the data once you are happy with it?
-## After we have discussed as a class, write out and run the workflow in tidyverse R
-## BONUS: Make a panel graph of C:N by nitrogen addition for each species
+## After we have discussed as a class, write out and run the workflow in tidyverse R.
+## BONUS: Make a panel graph of C:N by nitrogen addition for each species.
 
 ## Here's the data import to get you going:
 cdrCN <- read.csv("e001_Plant aboveground biomass carbon and nitrogen.csv", stringsAsFactors = F, skip = 491) %>%
@@ -162,16 +161,16 @@ cdrCN <- read.csv("e001_Plant aboveground biomass carbon and nitrogen.csv", stri
 
 ## WORKFLOW A: Mimic Figure 6a 
 ## Metadata here: http://www.cedarcreek.umn.edu/research/data/dataset?ple001
-## With a partner, write out the workflow you will use in English and put up a green sticky.
+## With a partner, write out the workflow you will use in English and put up a green sticky when done.
 # What QAQC will you do beforehand? 
 # How will you manipulate the data once you are happy with it?
-## After we have discussed as a class, write out and run the workflow in tidyverse R
+## After we have discussed as a class, write out and run the workflow in tidyverse R.
 
 
 ## WORKFLOW B: How do you hypothesize that biomass will change with nitrogen and species richness?
 ## Metadata here: http://www.cedarcreek.umn.edu/research/data/dataset?ple001
-## With a partner, write out the workflow you will use in English and put up a green sticky.
+## With a partner, write out the workflow you will use in English and put up a green sticky when done.
 # What QAQC will you do beforehand? 
 # How will you manipulate the data once you are happy with it?
-## After we have discussed as a class, write out and run the workflow in tidyverse R
+## After we have discussed as a class, write out and run the workflow in tidyverse R.
 
