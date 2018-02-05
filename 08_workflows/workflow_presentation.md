81a96bba5be66e9ce7bd8966f1e38eebc25273d2
Lauren Hallett <hallett@uoregon.edu>
2018-02-04 22:45:00 -0800

    workflow script and prez

---
+++ 08_workflows/workflow_presentation.md
@@ -0,0 +1,62 @@
+workflow_presentation
+========================================================
+author: 
+date: 
+autosize: true
+
+
+
+
+
+
+
+What is a workflow?
+========================================================
+
+*A series of steps necessary to complete a task*
+
+For example, I hate flight delays! I want to avoid days and times that have a lot of delays.
+
+To minimize my risk of flight delays, I should take a large dataset of flights, 
+
+then select those flights that had delays,
+
+then group those flights by date and hour, 
+
+then summarize by mean flight delay,
+
+then select those times with delays over 2 hours
+
+*Then avoid traveling on those days and times!*
+
+
+dplyr allows me to do just that with the "pipeline"
+========================================================
+
+
+x  %>%  f(y) -> f(x, y)
+
+hourly_delay <- flightdata  %>%  
+  filter(!is.na(dep_delay))  %>%  
+  group_by(date, hour)  %>%  
+  summarise(  
+    delay = mean(dep_delay),  
+    n = n())  %>%  
+  filter(n > 2)  
+  
+  
+dplyr
+========================================================
+
+
+- **filter:** keep rows that match a criteria
+
+- **select**: pick columns by name
+
+- **arrange:** reorder rows
+
+- **mutate:** add new variables
+
+- **summarize:** reduce variables to values
+
+- **+ group_by**
---
+++ 08_workflows/workflow_script.R
@@ -0,0 +1,211 @@
+################################################################################
+### ENVS 410/510                                                             ###
+### TRANSFORM: Grouping and piping                                           ### 
+################################################################################
+
+## OBJECTIVES:
+## To learn how to plan a workflow.
+## To implement a workflow using the pipeline.
+## To this end we'll be using the dplyr package.
+
+
+library(tidyverse)
+library(lubridate)
+
+### Let's pick back up where we left on on Wednesday:
+
+wtemp <- read.csv("CalispellCreekandTributaryTemperatures.csv", stringsAsFactors = FALSE) %>%
+  as_tibble()
+names(wtemp) <-c ("date", "time", "calispell_temp", "smalle_temp", "winchester_temp")
+wtemp <- mutate(wtemp, date2 = mdy(date), month = month(date2), year = year(date2))
+
+
+###################################
+## 6) dplyr tool number 6: group_by
+###################################
+
+## That last one was supposed to be a leading question. I don't think mean temperature is that insightful. 
+## I'm more interested in how temperature changes with month or year.
+## If we add the group_by function, summarize will give us the requested value FOR EACH GROUP.
+
+## First, let's create a new dataframe table that is equal to to wtemp2 but includes two grouping variables: month and year
+wtemp_by_monthyear <- group_by(wtemp, month, year)
+## QUESTION: Print wtemp and wtemp_by_month. Can you see how they differ?
+
+## Use summarize again, but this time on wtemp_by_month.
+summarize(wtemp_by_monthyear, avg_temp_calispell= mean(calispell_temp, na.rm=TRUE))
+
+## Let's take this a step further, and look at the mean and standard error.
+## First, run this code to store a function that calculates SE
+calcSE<-function(x){
+  x <- x[is.na(x)==F]
+  sd(x)/sqrt(length(x))
+}
+
+## Then, let's summarize Calispell Creek temperatures both by mean and se and store it as a new dataframe:
+wtemp_calispell_summary<- summarize(wtemp_by_monthyear, 
+                                    avg_temp_calispell= mean(calispell_temp, na.rm=TRUE),  
+                                    se_temp_calispell= calcSE(calispell_temp))
+
+## Notice how this gets our data into great shape for plotting. Just for fun, let's plot mean temp by month and include se
+ggplot(wtemp_calispell_summary, aes(x=month, y=avg_temp_calispell)) +
+  geom_bar(stat="identity", fill="grey") + 
+  geom_errorbar(aes(ymax = avg_temp_calispell + se_temp_calispell, 
+                    ymin = avg_temp_calispell - se_temp_calispell), width=.1) + 
+  xlab("Month") + ylab("Mean temperature at Calispell Creek (degrees C)") + 
+  facet_wrap(~year) 
+
+
+## One last note on group_by: If you group a dataframe table and then create a new variable with mutate, the value for that variable 
+## will be repeated for each member of the group. For example:
+mutate(wtemp_by_monthyear, avg_temp_calispell=mean(calispell_temp, na.rm=TRUE))
+
+## QUESTION: Can you think of a scenario in which this is what would want to do?
+
+
+#############################################
+## 7) dplyr tool number 7: %>% (the pipeline)
+#############################################
+
+## It took us a lot of steps to get from the raw data to that graph!
+## We had to: 
+## 0) turn the dataframe into a dataframe table
+## 1) select the date, time and Calispell temperature variable
+## 2) filter out the missing values from the temperature variable
+## 3) arrange by temperature (well, we didn't haaave to, but it was nice)
+## 4) mutate a whole lot! we created the date2 column (a date-time object) and used it to create a month and a year column
+## 5) group_by month and year
+## 6) summarize temperature by mean and se
+## 7) THEN graph
+
+## This is a "workflow" - and dplyr has a handy tool to make this process clear and easy.
+## It is called the "pipeline". Everytime a function is followed by the %>%,  it indicates that more work will be done on the object.
+## To illustrate, here is the pipeline to create a graphical object stored as "Calispell_monthlytemp" from the initial wtemp dataframe 
+
+Calispell_monthtemp <- wtemp %>%
+  as_tibble() %>%
+  select(date, time, calispell_temp) %>%
+  filter(!is.na(calispell_temp)) %>%
+  arrange(calispell_temp) %>%  
+  mutate(date2 = mdy(date), month = month(date2), year = year(date2)) %>%
+  group_by(month, year) %>%
+  summarize(avg_temp_calispell = mean(calispell_temp),  
+            se_temp_calispell = calcSE(calispell_temp)) %>%
+  ggplot(aes(x = month, y = avg_temp_calispell)) + 
+  geom_bar(stat="identity", fill="grey") + 
+  geom_errorbar(aes(ymax = avg_temp_calispell + se_temp_calispell, 
+                    ymin = avg_temp_calispell-se_temp_calispell), width=.1) + 
+  xlab("Month") + ylab("Mean temperature at Calispell Creek (degrees C)") +
+  facet_wrap(~year) 
+
+##  Nice!! Let's look at what we created:
+Calispell_monthtemp
+
+## QUESTION: Where is (are) the dataset specified in the pipeline?
+
+
+################################################################################
+#####                            YOUR TURN                              ########
+################################################################################
+
+################################################################################
+##### 1) SPECIES RICHNESS WITH N-DEPOSITION                                  ###
+################################################################################
+
+## Check out  http://www.cedarcreek.umn.edu/research/experiments/e001
+## What did Clark and Tilman 2008 find? See text and Figure 1
+## Q: What pattern did they observe between species richness and nitrogen treatment?
+## Q: What years and treatments did they include in Figure 1?
+
+## WORKFLOW: Mimic Figure 1 (except on the y-axis just plot species richness).
+## Metadata here: http://www.cedarcreek.umn.edu/research/data/dataset?ple001
+## With a partner, write out the workflow you will use in English and put up a green sticky.
+    # What QAQC will you do beforehand? 
+    # How will you manipulate the data once you are happy with it?
+## After we have discussed as a class, write out and run the workflow in tidyverse R
+
+## Here's the data import to get you going:
+cdrplant <- read.csv("e001_Plant aboveground biomass data.csv", stringsAsFactors = F, skip = 726) %>%
+  as.tibble()
+
+
+################################################################################
+##### 2) C:N RATIO WITH N-DEPOSITION                                         ###
+################################################################################
+
+## Check out  http://www.cedarcreek.umn.edu/research/experiments/e001
+## What did Wedin and Tilman 1996 find? See text and Figure 7
+
+## WORKFLOW: Mimic a panel in Figure 7 for C:N ratio of above ground biomass with nitrogen addition level
+## Metadata here: http://www.cedarcreek.umn.edu/research/data/dataset?nbe001
+## With a partner, write out the workflow you will use in English and put up a green sticky
+   # What QAQC will you do beforehand? 
+   # How will you manipulate the data once you are happy with it?
+## After we have discussed as a class, write out and run the workflow in tidyverse R
+## BONUS: Make a panel graph of C:N by nitrogen addition for each species
+
+## Here's the data import to get you going:
+cdrCN <- read.csv("e001_Plant aboveground biomass carbon and nitrogen.csv", stringsAsFactors = F, skip = 491) %>%
+  as.tibble()
+
+
+################################################################################
+##### 3) TOTAL BIOMASS WITH N-DEPOSTION AND RICHNESS                         ###
+################################################################################
+
+
+
+## remove burned plots (those are in field D, and the others from 2005-2009)
+exts <- cdrplant %>%
+  select(NTrt, NAdd, NitrAdd, NAtm.NAdd) %>%
+  unique()
+
+rich <- cdrplant %>%
+  group_by(Exp, Year, Field, Plot, NTrt, NAdd) %>%
+  filter(Biomass > 0) %>%
+  count() %>%
+  filter(Year < 2005) %>%
+   group_by(Exp, Year, NTrt, NAdd) %>%
+  summarize(meanrich = mean(n))
+
+ggplot(rich, aes(x=Year, y=meanrich, color = as.factor(NAdd), group = NTrt)) + geom_point() + geom_line()
+
+
+rich <- cdrplant %>%
+  group_by(Exp, Year, Field, Plot, NTrt, NAdd) %>%
+  filter(Biomass > 0) %>%
+  summarize(rich = n(), totbiomass = sum(Biomass)) %>%
+  filter(Year < 2005) %>%
+  group_by(Exp, Year, NTrt, NAdd) %>%
+  summarize(meanrich = mean(rich), meanbiomass = mean(totbiomass), CV = sd(totbiomass, na.rm = T)/mean(totbiomass, na.rm=T))
+
+ggplot(subset(rich, Year < 1996), aes(x=meanrich, y=meanbiomass, color = NTrt)) + geom_point() + 
+  facet_wrap(~NTrt,scales = "free") + geom_smooth(method = "lm")
+
+
+rich <- cdrplant %>%
+  group_by(Exp, Year, Field, Plot, NTrt, NAdd) %>%
+  filter(Biomass > 0) %>%
+  summarize(rich = n(), totbiomass = sum(Biomass)) 
+
+%>%
+  filter(Year < 2005) %>%
+  group_by(Year, Exp, Year, NTrt, NAdd) %>%
+  summarize(meanrich = mean(rich), meanbiomass = mean(totbiomass))
+
+ggplot(subset(rich, Year < 1996), aes(x=rich, y=totbiomass, color = NTrt)) + geom_point() + 
+  facet_wrap(~NTrt,scales = "free") + geom_smooth(method = "lm")
+
+
+
+rich <- cdrplant %>%
+  group_by(Exp, Year, Field, Plot, NTrt, NAdd) %>%
+  filter(Biomass > 0) %>%
+  summarize(rich = n(), totbiomass = sum(Biomass)) %>%
+  filter(Year < 2005) %>%
+  group_by(Exp, Plot, Field, NTrt, NAdd) %>%
+  summarize(meanrich = mean(rich), meanbiomass = mean(totbiomass), CVbiomass = meanbiomass/sd(totbiomass))
+
+ggplot(rich, aes(x=meanrich, y=meanbiomass, color = NTrt)) + geom_point() #+ 
+  facet_wrap(~NTrt,scales = "free") + geom_smooth(method = "lm")
+
